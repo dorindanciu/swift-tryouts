704cfd37df3eab5f03252a5df17f31e7c653a93b
Dorin Danciu <1619134+dorindanciu@users.noreply.github.com>
2024-07-16 16:04:14 +0300

    Add swift package manifest

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-tryouts",
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .macCatalyst(.v18)]
+)
