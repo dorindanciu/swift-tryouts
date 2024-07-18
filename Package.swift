934a71f570577ca255ebc5a3b13ffc458aca5567
Dorin Danciu <1619134+dorindanciu@users.noreply.github.com>
2024-07-18 20:24:38 +0300

    Add custom target factory methods

--- Package.swift
+++ Package.swift
@@ -7,3 +7,42 @@ let package = Package(
     name: "swift-tryouts",
     platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .macCatalyst(.v18)]
 )
+
+extension PackageDescription.Target {
+
+    /// Creates a regular target.
+    /// - Parameters:
+    ///   - name: The name of the target. It will be used to derive the custom path for the target.
+    ///   - dependencies: The dependencies of the target. A dependency can be another target in the package or a product from a package dependency.
+    static func tryoutTarget(name: String, dependencies: [Target.Dependency] = []) -> Target {
+        .target(name: name, dependencies: dependencies, path: sourcesPath(for: name))
+    }
+
+    /// Creates a test target.
+    /// - Parameters:
+    ///   - name: The name of the target. It will be used to derive the custom path for the target.
+    ///   - dependencies: The dependencies of the target. A dependency can be another target in the package or a product from a package dependency.
+    static func tryoutTestTarget(name: String, dependencies: [Target.Dependency] = []) -> Target {
+        .testTarget(name: name, dependencies: dependencies, path: testsPath(for: name))
+    }
+
+    private static func sourcesPath(for name: String) -> String {
+        return "Tryouts/\(name)/Sources"
+    }
+
+    private static func testsPath(for name: String) -> String {
+        // Compute the name of the hosting folder from the target name.
+        let name: String = {
+            switch try? /(.*)Tests$/.wholeMatch(in: name) {
+            case .none:
+                // Default to the provided name.
+                return name
+            case .some(let match):
+                // Return the extracted prefix.
+                return String(match.output.1)
+            }
+        }()
+
+        return "Tryouts/\(name)/Tests"
+    }
+}
